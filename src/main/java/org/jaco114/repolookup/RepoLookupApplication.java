c5853a39671722ef5723466b1c9da539a5823e65
Jacek <minkiel.jacek@gmail.com>
2024-02-13 17:42:40 +0100

    initial commit

---
+++ src/main/java/org/jaco114/repolookup/RepoLookupApplication.java
@@ -0,0 +1,13 @@
+package org.jaco114.repolookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoLookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoLookupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jaco114/repolookup/RepoLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jaco114.repolookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoLookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
